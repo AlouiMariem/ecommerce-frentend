87c8b69f9928aa95a153c8d648e0df7a87fd8fc2
AlouiMariem <[200~ --global user.name Your>
2023-06-09 21:11:22 +0200

    first commit

---
+++ constants/Network.js
@@ -0,0 +1,10 @@
+
+
+const HOST = '192.168.1.97'
+export default network = {
+  serverip: `http://${HOST}:3000`,
+};
+// environment.js
+
+
+// make sure to export the variable so you can import it in other files
